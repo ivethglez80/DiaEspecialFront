8f0cf35d65e2e589e1bb0409ead5d36e82a05bf7
ivethG <ivethglez80@gmail.com>
2024-03-15 10:57:54 -0300

    catalogo fixed

--- src/views/catalogo/catalogoFull.jsx
+++ src/views/catalogo/catalogoFull.jsx
@@ -11,14 +11,14 @@ const CatalogoFull = () =>{
         <>
         <div className="h-screen">
         <NavBarC />
-            <div className="relative h-screen overflow-hidden">
+            <div className="relative h-screen overflow-hidden pb-10">
                 <img src={bgGray} alt="backgroudn roses gray" className="absolute object-cover top-0 left-0 h-screen w-screen"/>
-                <div className="relative pt-10">
+                <div className="relative pt-8">
                 <NavCatalogo />
                 </div>
 
-                <div className="relative flex justify-center">
-                <div className="h-[370px] overflow-y-auto sm:w-11/12">
+                <div className=" h-full overflow-y-auto relative flex justify-center bg-red-300">
+                <div className="sm:w-11/12">
                     <CardsContainer />
                 </div>
                 </div>
